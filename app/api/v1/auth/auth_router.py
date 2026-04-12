2bd95426a1a908ba7718126ba23d3ce76eadc1d9
Meshrr <volframhome@gmail.com>
2026-04-12 16:22:52 +0500

    /register endpoint + auth router

---
+++ app/api/v1/auth/auth_router.py
@@ -0,0 +1,51 @@
+from fastapi import Depends, APIRouter, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.schemas import RegisterRequest, LoginRequest, TokenResponse
+from app.models import User
+
+from app.api.v1.auth.auth_utils import (
+    encode_jwt,
+    decode_jwt,
+    get_current_user,
+    validate_password,
+    hash_password)
+from database import get_db
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+@router.post("/register", response_model=TokenResponse)
+async def register(user_data: RegisterRequest,
+                   db: AsyncSession = Depends(get_db)):
+
+    check_email = await db.execute(select(User).where(User.email == user_data.email))
+
+    result_email = check_email.scalar_one_or_none()
+    if result_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='This email is already registered')
+
+    check_username = await db.execute(select(User).where(User.username == user_data.username))
+    result_username = check_username.scalar_one_or_none()
+    if result_username:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='This username is already registered')
+
+    hashed_password = hash_password(user_data.password)
+
+    user = User(username=user_data.username,
+                email=user_data.email,
+                hashed_password=hashed_password)
+
+    db.add(user)
+    await db.commit()
+    await db.refresh(User)
+
+    user_payload = {
+        "sub": str(user.id),
+        "email": user.email,
+        "username": user.username
+    }
+
+    access_token = encode_jwt(payload=user_payload)
+
+    return TokenResponse(access_token=access_token, token_type="bearer")
