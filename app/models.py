7277c72b1394084ad5dd053f85f8347445b9d2e6
Meshrr <volframhome@gmail.com>
2026-04-15 23:41:59 +0500

    add parent_id to categories

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 import uuid
 
 from datetime import datetime
-from sqlalchemy import String, Integer, DateTime, func, Float, Boolean, JSON, UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from tkinter.constants import CASCADE
+from typing import List
+
+from sqlalchemy import String, Integer, DateTime, func, Float, Boolean, JSON, UUID, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
@@ -32,16 +35,26 @@ class User(Base):
     email_verified: Mapped[bool] = mapped_column(Boolean, default=False)
 
 
+#сделаем вложенность категорий с через родительский айди, ссылающийся на айди этой же таблицы.
+#к примеру категория Одежда будет родительской для категории Пуховики
 class Category(Base):
     __tablename__ = "category"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, index=True)
+
+    parent_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey("category.id", ondelete="CASCADE"), index=True, nullable=True)
 
     name: Mapped[str] = mapped_column(String, nullable=False)
 
     description: Mapped[str] = mapped_column(String, nullable=False)
 
     created_at: Mapped[int] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+
+
+    parent: Mapped["Category | None"] = relationship("Category", back_populates="children", remote_side=[id])
+
+    children: Mapped[List["Category"]] = relationship("Category", back_populates="parent")
 
 
 
