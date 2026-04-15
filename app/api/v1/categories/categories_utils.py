bfb268b3ae9d2458b80928f2ce6850e1b6e3d9e3
Meshrr <volframhome@gmail.com>
2026-04-15 23:51:03 +0500

    add endpoint to check children of category

--- app/api/v1/categories/categories_utils.py
+++ app/api/v1/categories/categories_utils.py
@@ -37,3 +37,18 @@ async def get_category(category_id: str,
         )
 
     return category
+
+@router.get("/{category_id}/tree", response_model=List[CategoryResponse])
+async def get_category_tree(category_id: int, db: AsyncSession = Depends(get_db)):
+
+    check_category = await db.execute(select(Category).where(Category.id == category_id))
+
+    category = check_category.scalar_one_or_none()
+    if not category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+
+    category_children = await db.execute(select(Category.children).where(Category.parent_id == category_id))
+
+    result = category_children.scalars().all()
+
+    return result
